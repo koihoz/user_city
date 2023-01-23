3df332dcb8d1b0dc7122def0d9c161f4fda66f64
koihoz <koihoz@mail.ru>
2023-01-23 15:18:00 +0100

    rechecking

---
+++ src/main/java/org/example/model/User.java
@@ -0,0 +1,54 @@
+package org.example.model;
+
+import java.util.Objects;
+import java.util.Optional;
+import java.util.Set;
+
+public class User {
+    private final Long id;
+    private final String name;
+    private final Set<String> citiesWhereLived;
+    private final Set<String> citiesWhereWorked;
+
+    //request
+    public User(String name, Set<String> citiesWhereLived, Set<String> citiesWhereWorked) {
+        this.id = null;
+        this.name = name;
+        this.citiesWhereLived = citiesWhereLived;
+        this.citiesWhereWorked = citiesWhereWorked;
+    }
+
+    //response
+    public User(Long id, String name, Set<String> citiesWhereLived, Set<String> citiesWhereWorked) {
+        this.id = Objects.requireNonNull(id);
+        this.name = Objects.requireNonNull(name);
+        this.citiesWhereLived = Objects.requireNonNull(citiesWhereLived);
+        this.citiesWhereWorked = Objects.requireNonNull(citiesWhereWorked);
+    }
+
+    public Optional<Long> getId() {
+        return Optional.ofNullable(id); //потом разберем
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Set<String> getCitiesWhereLived() {
+        return citiesWhereLived;
+    }
+
+    public Set<String> getCitiesWhereWorked() {
+        return citiesWhereWorked;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", citiesWhereLived=" + citiesWhereLived +
+                ", citiesWhereWorked=" + citiesWhereWorked +
+                '}';
+    }
+}
